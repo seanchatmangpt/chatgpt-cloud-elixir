a18dd02688abae887074bd1e00b90f7dcf6c4b7d
Sean Chatman <136349053+seanchatmangpt@users.noreply.github.com>
2026-08-25 00:03:09 -0700

    style(control-plane): apply canonical runtime format

--- control-plane/config/runtime.exs
+++ control-plane/config/runtime.exs
@@ -56,9 +56,7 @@ if config_env() == :prod do
     ciphers: [
       default: {
         Cloak.Ciphers.AES.GCM,
-        tag: "AES.GCM.V1",
-        key: decoded_cloak_key,
-        iv_length: 12
+        tag: "AES.GCM.V1", key: decoded_cloak_key, iv_length: 12
       }
     ]
 
