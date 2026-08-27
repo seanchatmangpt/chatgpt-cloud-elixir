7aa3ce9587d2ff238ca19bd992b05a19c5582ecf
Sean Chatman <136349053+seanchatmangpt@users.noreply.github.com>
2026-08-27 14:05:22 -0700

    fix(runtime): define encrypted secret envelope type

--- control-plane/lib/chatgpt_cloud_control_plane/runtime_integration/secret_field_envelope.ex
+++ control-plane/lib/chatgpt_cloud_control_plane/runtime_integration/secret_field_envelope.ex
@@ -4,13 +4,15 @@ defmodule ChatGPTCloud.RuntimeIntegration.SecretFieldEnvelope do
   @enforce_keys [:field, :ciphertext_ref, :key_version]
   defstruct [:field, :ciphertext_ref, :key_version]
 
-  @spec admit(t()) :: :ok | {:error, :invalid_secret_envelope} when t: %__MODULE__{}
+  @type t :: %__MODULE__{field: atom(), ciphertext_ref: String.t(), key_version: pos_integer()}
+
+  @spec admit(t()) :: :ok | {:error, :invalid_secret_envelope}
   def admit(%__MODULE__{field: field, ciphertext_ref: ref, key_version: version})
       when is_atom(field) and is_binary(ref) and ref != "" and is_integer(version) and version > 0,
       do: :ok
 
   def admit(_), do: {:error, :invalid_secret_envelope}
 
-  @spec receipt_projection(t()) :: map() when t: %__MODULE__{}
+  @spec receipt_projection(t()) :: map()
   def receipt_projection(%__MODULE__{field: field, key_version: version}), do: %{field: field, encrypted: true, key_version: version}
 end
