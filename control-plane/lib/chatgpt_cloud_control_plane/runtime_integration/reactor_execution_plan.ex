31f4681c8648361a01128618e53fb4f5c7af79c4
Sean Chatman <136349053+seanchatmangpt@users.noreply.github.com>
2026-08-27 14:04:57 -0700

    fix(runtime): define Reactor execution plan type

--- control-plane/lib/chatgpt_cloud_control_plane/runtime_integration/reactor_execution_plan.ex
+++ control-plane/lib/chatgpt_cloud_control_plane/runtime_integration/reactor_execution_plan.ex
@@ -5,9 +5,13 @@ defmodule ChatGPTCloud.RuntimeIntegration.ReactorExecutionPlan do
   @enforce_keys [:steps]
   defstruct [:steps]
 
-  @spec admit(t()) :: :ok | {:error, :invalid_reactor_plan} when t: %__MODULE__{}
+  @type t :: %__MODULE__{steps: [atom()]}
+
+  @spec admit(t()) :: :ok | {:error, :invalid_reactor_plan}
   def admit(%__MODULE__{steps: steps}) when is_list(steps) and steps != [] do
-    if Enum.all?(steps, &(&1 in @allowed)) and length(steps) == length(Enum.uniq(steps)), do: :ok, else: {:error, :invalid_reactor_plan}
+    if Enum.all?(steps, &(&1 in @allowed)) and length(steps) == length(Enum.uniq(steps)),
+      do: :ok,
+      else: {:error, :invalid_reactor_plan}
   end
 
   def admit(_), do: {:error, :invalid_reactor_plan}
